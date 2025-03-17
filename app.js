36cf3172170a2ef2c44fbc20e9cab37637436403
Emiliano <emisanguillen@gmail.com>
2025-03-16 23:56:10 -0600

    Primera corrección

--- app.js
+++ app.js
@@ -6,30 +6,48 @@ const inpAmigo = document.getElementById("amigo");
 const ulListaAmigos = document.getElementById("listaAmigos");
 
 // Resultado del sorteo.
-const ulResultado = document.getElementById("resultado")
+const ulResultado = document.getElementById("resultado");
 
 
 // Función que agrega amigos a la lista.
 function agregarAmigo() {
 
+    // Alerta si no hay amigos.
     if (!inpAmigo.value) {
         alert("Escriba el nombre de algún amigo.")
     };
 
+    // Agrega amigos a la lista.
     listaAmigos.push(inpAmigo.value);
     ulListaAmigos.innerHTML += `<li>${inpAmigo.value}</li>`;
+
+    // Limpia el cuadro de int.
+    function limpiar() {
+        inpAmigo.value = ""
+    };
+    limpiar();
 
 };
 
 // Función para sortear amigo secreto.
 function sortearAmigo() {
 
-    const random = Math.floor((Math.random() * listaAmigos.length));
+    // Alerta en caso de que no haya amigos en la lista.
+    if (listaAmigos.length == 0) {
+        alert("No hay amigos para sortear.")
+    }
+    else {
+        // Generar valor random.
+        const random = Math.floor((Math.random() * listaAmigos.length));
 
-    // Verifica valor random.
-    console.log(random);
+        // Verifica valor random.
+        console.log(random);
 
-    const amigoSecreto = listaAmigos[random];
-    ulResultado.innerHTML = `<li>${amigoSecreto}</li>`
+        // Arroja amigo secreto.
+        const amigoSecreto = listaAmigos[random];
+        ulResultado.innerHTML = `<li>Su amigo secreto es: ${amigoSecreto} !!!</li>`
 
-}
+    };
+    
+};
+
